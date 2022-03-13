ba705d950bc54bd76aa9da33e3695453c1ca7886
Piotr Daniel <piotrmaciejdaniel@gmail.com>
2022-03-13 18:07:56 +0100

    successfully moved to up-to-date version rtic crate

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,35 @@
 extern crate panic_semihosting;
 
 use cortex_m::asm::delay;
+use systick_monotonic::*;
 use embedded_hal::digital::v2::OutputPin;
-use rtic::app;
 use stm32f1xx_hal::prelude::*;
 use stm32f1xx_hal::usb::{Peripheral, UsbBus, UsbBusType};
 use usb_device::bus;
 use usb_device::prelude::*;
 use usbd_serial::{SerialPort, USB_CLASS_CDC};
 
+use rtic::app;
+
 #[app(device = stm32f1xx_hal::stm32, peripherals = true)]
-const APP: () = {
-    struct Resources {
-        usb_dev: UsbDevice<'static, UsbBusType>,
-        serial: SerialPort<'static, UsbBusType>,
+mod app {
+    use super::*;
+
+    #[monotonic(binds=SysTick, default=true)]
+    type SysMono = Systick<1_000>; // 1 kHz / 1ms granularity
+
+    #[shared]
+    struct SharedResources {   
     }
 
-    #[init]
-    fn init(cx: init::Context) -> init::LateResources {
-        static mut USB_BUS: Option<bus::UsbBusAllocator<UsbBusType>> = None;
+    #[local]
+    struct LocalResources {
+        usb_dev: UsbDevice<'static, UsbBusType>,
+        serial: SerialPort<'static, UsbBusType>,     
+    }
+
+    #[init(local = [USB_BUS: Option<bus::UsbBusAllocator<UsbBusType>> = None])]
+    fn init(cx: init::Context) -> (SharedResources, LocalResources,  init::Monotonics) {
 
         let mut flash = cx.device.FLASH.constrain();
         let mut rcc = cx.device.RCC.constrain();
@@ -34,6 +45,9 @@ const APP: () = {
             .freeze(&mut flash.acr);
 
         assert!(clocks.usbclk_valid());
+
+        // Initialize the monotonic
+        let mono = Systick::new(cx.core.SYST, clocks.sysclk().0);
 
         let mut gpioa = cx.device.GPIOA.split(&mut rcc.apb2);
 
@@ -51,52 +65,56 @@ const APP: () = {
             pin_dp: usb_dp,
         };
 
-        *USB_BUS = Some(UsbBus::new(usb));
+        *cx.local.USB_BUS = Some(UsbBus::new(usb));
 
-        let serial = SerialPort::new(USB_BUS.as_ref().unwrap());
+        let serial = SerialPort::new(cx.local.USB_BUS.as_ref().unwrap());
 
-        let usb_dev = UsbDeviceBuilder::new(USB_BUS.as_ref().unwrap(), UsbVidPid(0x1600, 0x2137))
+        let usb_dev = UsbDeviceBuilder::new(cx.local.USB_BUS.as_ref().unwrap(), UsbVidPid(0x1600, 0x2137))
             .manufacturer("Sinara")
             .product("DiPho")
             .serial_number("DiPho")
             .device_class(USB_CLASS_CDC)
             .build();
 
-        init::LateResources { usb_dev, serial }
+            (
+                SharedResources {
+                },
+                LocalResources {
+                    usb_dev,
+                    serial
+                },
+                init::Monotonics(mono),
+            )
     }
 
-    #[task(binds = USB_HP_CAN_TX, resources = [usb_dev, serial])]
-    fn usb_tx(mut cx: usb_tx::Context) {
-        usb_poll(&mut cx.resources.usb_dev, &mut cx.resources.serial);
-    }
-
-    #[task(binds = USB_LP_CAN_RX0, resources = [usb_dev, serial])]
+    #[task(binds = USB_LP_CAN_RX0, local = [usb_dev, serial])]
     fn usb_rx0(mut cx: usb_rx0::Context) {
-        usb_poll(&mut cx.resources.usb_dev, &mut cx.resources.serial);
-    }
-};
-
-fn usb_poll<B: bus::UsbBus>(
-    usb_dev: &mut UsbDevice<'static, B>,
-    serial: &mut SerialPort<'static, B>,
-) {
-    if !usb_dev.poll(&mut [serial]) {
-        return;
+        usb_poll(&mut cx.local.usb_dev, &mut cx.local.serial);
     }
 
-    let mut buf = [0u8; 8];
-
-    match serial.read(&mut buf) {
-        Ok(count) if count > 0 => {
-            // Echo back in upper case
-            for c in buf[0..count].iter_mut() {
-                if 0x61 <= *c && *c <= 0x7a {
-                    *c &= !0x20;
-                }
-            }
-
-            serial.write(&buf[0..count]).ok();
+    fn usb_poll<B: bus::UsbBus>(
+        usb_dev: &mut UsbDevice<'static, B>,
+        serial: &mut SerialPort<'static, B>,
+    ) {
+        if !usb_dev.poll(&mut [serial]) {
+            return;
         }
-        _ => {}
+    
+        let mut buf = [0u8; 8];
+    
+        match serial.read(&mut buf) {
+            Ok(count) if count > 0 => {
+                // Echo back in upper case
+                for c in buf[0..count].iter_mut() {
+                    if 0x61 <= *c && *c <= 0x7a {
+                        *c &= !0x20;
+                    }
+                }
+    
+                serial.write(&buf[0..count]).ok();
+            }
+            _ => {}
+        }
     }
 }
+
