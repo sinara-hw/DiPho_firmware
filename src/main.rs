21f004fa91b045ddc80dbc562425da2010440dbf
Piotr Daniel <piotrmaciejdaniel@gmail.com>
2022-05-15 16:03:11 +0200

    serial terminal settings proof of concept works

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ use usb_device::prelude::*;
 pub struct DeviceSettings {
     pub sampling_time: SampleTime,
     pub sampling_freq: Hertz<u32>,
-    pub data_interval: systick_monotonic::fugit::Duration<u64, 1, 1000>,
+    //pub data_interval: systick_monotonic::fugit::Duration<u64, 1, 1000>,
     pub gain: GainSetting,
     pub led_off: bool,
 }
@@ -47,7 +47,7 @@ impl Default for DeviceSettings {
         Self {
             sampling_time: SampleTime::T_239,
             sampling_freq: 10.kHz(),
-            data_interval: (1000 as u64).millis(),
+            //data_interval: (1000 as u64).millis(),
             gain: GainSetting::Low,
             led_off: true,
         }
@@ -91,7 +91,7 @@ mod app {
 
         let dipho = hardware::setup::setup(cx.device);
 
-        data_task::spawn(mono.now()).unwrap();
+        //data_task::spawn(mono.now()).unwrap();
 
         let local = Local {
             adc_internal: dipho.adc_internal,
@@ -120,47 +120,49 @@ mod app {
         );
     }
 
-    #[task(binds = USB_HP_CAN_TX, shared = [usb_dev, serial_config, serial_data])]
+    #[task(binds = USB_HP_CAN_TX, shared = [usb_dev, serial_config, serial_data, device_settings])]
     fn usb_tx(cx: usb_tx::Context) {
         let mut usb_dev = cx.shared.usb_dev;
         let mut serial_config = cx.shared.serial_config;
         let mut serial_data = cx.shared.serial_data;
+        let mut device_settings = cx.shared.device_settings;
 
-        (&mut usb_dev, &mut serial_config, &mut serial_data).lock(
-            |usb_dev, serial_config, serial_data| {
-                super::usb_poll(usb_dev, serial_config, serial_data);
+        (&mut usb_dev, &mut serial_config, &mut serial_data, &mut device_settings).lock(
+            |usb_dev, serial_config, serial_data, device_settings| {
+                super::usb_poll(usb_dev, serial_config, serial_data, device_settings);
             },
         );
     }
 
-    #[task(binds = USB_LP_CAN_RX0, shared = [usb_dev, serial_config, serial_data])]
+    #[task(binds = USB_LP_CAN_RX0, shared = [usb_dev, serial_config, serial_data, device_settings])]
     fn usb_rx0(cx: usb_rx0::Context) {
         let mut usb_dev = cx.shared.usb_dev;
         let mut serial_config = cx.shared.serial_config;
         let mut serial_data = cx.shared.serial_data;
+        let mut device_settings = cx.shared.device_settings;
 
-        (&mut usb_dev, &mut serial_config, &mut serial_data).lock(
-            |usb_dev, serial_config, serial_data| {
-                super::usb_poll(usb_dev, serial_config, serial_data);
+        (&mut usb_dev, &mut serial_config, &mut serial_data, &mut device_settings).lock(
+            |usb_dev, serial_config, serial_data, device_settings| {
+                super::usb_poll(usb_dev, serial_config, serial_data, device_settings);
             },
         );
     }
 
-    #[task(priority = 1, shared=[device_settings])]
-    fn data_task(mut cx: data_task::Context, instant: <Mono as rtic::Monotonic>::Instant) {
-        let data_interval = cx
-            .shared
-            .device_settings
-            .lock(|device_settings| device_settings.data_interval);
-        let next_instant = instant + data_interval;
-        data_task::spawn_at(next_instant, next_instant).unwrap();
-    }
+    // #[task(priority = 1, shared=[device_settings])]
+    // fn update_settings_task(mut cx: data_task::Context, instant: <Mono as rtic::Monotonic>::Instant) {
+    //     &mut device_settings.lock(device_settings){
+    //         let i = 0
+    //     }
+    //     let next_instant = instant + (50 as u64).millis();
+    //     data_task::spawn_at(next_instant, next_instant).unwrap();
+    // }
 }
 
 fn usb_poll<B: usb_device::bus::UsbBus>(
     usb_dev: &mut usb_device::prelude::UsbDevice<'static, B>,
     serial_config: &mut usbd_serial::SerialPort<'static, B>,
     serial_data: &mut usbd_serial::SerialPort<'static, B>,
+    device_settings: &mut DeviceSettings
 ) {
     if !usb_dev.poll(&mut [serial_config, serial_data]) {
         return;
@@ -175,30 +177,58 @@ fn usb_poll<B: usb_device::bus::UsbBus>(
             let mut cfg_str = str::from_utf8(&buf).unwrap();
             let mut cfg_str_split = cfg_str.split_ascii_whitespace();
 
+            let af : ArrForm<64>;
             let response: &str;
-            match cfg_str_split.as_str() {
+            match cfg_str_split.next().unwrap_or_else(|| "") {
                 "help" => response = "<help text>\r\n",
                 "get" =>  {
-                    cfg_str_split.next();
-                    match cfg_str_split.as_str() {
+                    match cfg_str_split.next().unwrap_or_else(|| "") {
                         "afe_type" => response = "<afe type>\r\n",
-                        "gain" => response = "<gain setting>\r\n",
-                        "sampling_time" => response = "<sampling time setting>\r\n",
-                        "sampling_freq" => response = "<sampling frequency setting>\r\n",
-                        _ => response = "command error\r\n",
+                        "gain" => {
+                            af = arrform!(64, "gain setting: {:?} \r\n", device_settings.gain);
+                            response = af.as_str();
+                            },
+                        "sampling_time" => {
+                            af = arrform!(64, "gain setting: {:?} \r\n", device_settings.sampling_time);
+                            response = af.as_str();
+                            },
+                        "sampling_freq" => {
+                            af = arrform!(64, "gain setting: {:?} \r\n", device_settings.sampling_freq);
+                            response = af.as_str();
+                            },
+                        _ => response = "get command error: unknown field!\r\n",
                     };
                 },
                 "set" => {
-                    cfg_str_split.next();
-                    match cfg_str_split.as_str() {
+                    match cfg_str_split.next().unwrap_or_else(|| "") {
                         "afe_type" => response = "<afe type\r\n",
-                        "gain" => response = "<gain setting>\r\n",
+                        "gain" => {
+                            match cfg_str_split.next().unwrap_or_else(|| "") {
+                                "Low" | "low" | "LOW" | "lo" | "LO" => {
+                                    response = "set gain: \"Low\" ok!\r\n";
+                                    device_settings.gain = GainSetting::Low;
+                                    },
+                                "Medium" | "medium" | "MEDIUM" | "Med" | "med" | "MED" => {
+                                    response = "set gain: \"Medium\" ok!\r\n";
+                                    device_settings.gain = GainSetting::Medium;
+                                    },
+                                "High" | "high" | "HIGH" | "hi" | "HI" => {
+                                    response = "set gain: \"High\" ok!\r\n";
+                                    device_settings.gain = GainSetting::High;
+                                    },
+                                "Max" | "max" | "MAX" => {
+                                    response = "set gain: \"Max\" ok!\r\n";
+                                    device_settings.gain = GainSetting::Max;
+                                    },
+                                _ => response = "set gain error: invalid value!\r\n",
+                            }
+                            },
                         "sampling_time" => response = "<sampling time setting>\r\n",
                         "sampling_freq" => response = "<sampling frequency setting>\r\n",
-                        _ => response = "command error\r\n",
+                        _ => response = "set command error: unknown field!\r\n",
                     };
                 },
-                _ => response = cfg_str_split.as_str(),
+                _ => response = "unknown command",
 
             }
 
